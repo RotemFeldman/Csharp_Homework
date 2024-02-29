92204887ab818758f618de5cac67dae46f2fe6f3
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2024-02-29 22:36:40 +0200

    forms calculator

---
+++ FormsHomework_RotemFeldman/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FormsHomework_RotemFeldman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Calculate_Button = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.operation_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(212, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Add",
+            "Subtract",
+            "Multiply"});
+            this.comboBox1.Location = new System.Drawing.Point(329, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Calculate_Button
+            // 
+            this.Calculate_Button.Location = new System.Drawing.Point(105, 162);
+            this.Calculate_Button.Name = "Calculate_Button";
+            this.Calculate_Button.Size = new System.Drawing.Size(171, 60);
+            this.Calculate_Button.TabIndex = 3;
+            this.Calculate_Button.Text = "Calculate";
+            this.Calculate_Button.UseVisualStyleBackColor = true;
+            this.Calculate_Button.Click += new System.EventHandler(this.Calculate_Button_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(367, 182);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 20);
+            this.ResultLabel.TabIndex = 4;
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ResultLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // operation_label
+            // 
+            this.operation_label.AutoSize = true;
+            this.operation_label.Location = new System.Drawing.Point(184, 81);
+            this.operation_label.Name = "operation_label";
+            this.operation_label.Size = new System.Drawing.Size(27, 20);
+            this.operation_label.TabIndex = 5;
+            this.operation_label.Text = "op";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 294);
+            this.Controls.Add(this.operation_label);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.Calculate_Button);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Calculate_Button;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label operation_label;
+    }
+}
+
---
+++ FormsHomework_RotemFeldman/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static FormsHomework_RotemFeldman.Form1;
+
+namespace FormsHomework_RotemFeldman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "Add";
+            operation_label.Text = "+";
+        }
+
+        public delegate float Calculate(float x, float y);
+
+        private float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        private float Subtract(float x, float y)
+        {
+            return x - y;
+        }
+
+        private float Multiply(float x, float y)
+        {
+            return x * y;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Button_Click(object sender, EventArgs e)
+        {
+            float num1 = float.Parse(textBox1.Text);
+            float num2 = float.Parse(textBox2.Text);
+            Calculate calculate = Add;
+          
+
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Add":
+                    calculate = Add;
+                    break;
+                case "Subtract":
+                    calculate = Subtract;
+                    break;
+                case "Multiply":
+                    calculate = Multiply;
+                    break;
+            }
+
+            ResultLabel.Text = calculate(num1,num2).ToString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains('.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains('.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string operation = "+";
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    operation = "+";
+                    break;
+                case 1:
+                    operation = "-";
+                    break;
+                case 2:
+                    operation= "*";
+                    break;
+            }
+
+            operation_label.Text = operation;
+        }
+        
+    }
+}
