bdcc12dec2231b979989659dff8a7a56c0a50064
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2024-02-29 18:15:57 +0200

    limited observable list

---
+++ LimitedObservableList/LimitedObservableList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitedObservableList
+{
+    public class LimitedObservableList
+    {
+        public event ListChanged? OnListChanged;
+        public delegate string ListChanged (object sender, string changeMade);
+
+        private ListChanged listChangedDelegate;
+
+        private Predicate<string> _predicate;
+        private List<string> _list = new List<string>();
+
+        public LimitedObservableList(Predicate<string> predicate)
+        {
+            _predicate = predicate;           
+            listChangedDelegate += NotifyOnDelegateCall;
+        }
+
+        public bool TryAdd(string item)
+        {
+            if(_predicate(item))
+            {
+                _list.Add(item);
+                OnListChanged?.Invoke(this, item);
+                listChangedDelegate?.Invoke(this, item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PrintAll()
+        {
+            foreach(var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private string NotifyOnDelegateCall(object sender, string changeMade)
+        {
+            Console.WriteLine("listChangedDelegate called");
+            
+            return changeMade;
+        }
+
+        
+    }
+
+}
---
+++ LimitedObservableList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace LimitedObservableList
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            bool ContainsS(string s)
+            {
+                s.ToLower();
+                return s.Contains("s");
+            }
+
+            Predicate<string> Predicate = ContainsS;
+
+            var limitedObservableList = new LimitedObservableList(Predicate);
+
+            limitedObservableList.TryAdd("salad");
+            limitedObservableList.TryAdd("lisbon");
+            limitedObservableList.TryAdd("orangutang");
+            limitedObservableList.TryAdd("computer");
+            limitedObservableList.TryAdd("plaster");
+            limitedObservableList.TryAdd("wood");
+            limitedObservableList.TryAdd("evening");
+            limitedObservableList.TryAdd("glue");
+            limitedObservableList.TryAdd("hard drive");
+            limitedObservableList.TryAdd("empty");
+
+            limitedObservableList.PrintAll();
+            
+        }
+    }
+}
